ad7c486bf493bb06ecf2d59e5adf62e3f9e93565
Edward <edwardhdavies@outlook.com>
2023-05-20 21:44:42 +0100

    Site configuration

--- Setting up Website commands.R
+++ Setting up Website commands.R
@@ -5,3 +5,10 @@ blogdown::install_hugo()
 install.packages("rtools")
 
 install.packages("xfun")
+
+blogdown::new_site(theme = "MarcusVirg/forty", 
+                   sample = TRUE, 
+                   theme_example = TRUE,            
+                   empty_dirs = TRUE,            
+                   to_yaml = TRUE)
+
